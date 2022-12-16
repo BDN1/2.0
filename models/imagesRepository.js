623570c5ea95c8a2b6b6d46931e0d5fc94c8454f
erick <erick.boucherbeaudin@gmail.com>
2022-12-16 14:22:30 -0500

    first commit

---
+++ models/imagesRepository.js
@@ -0,0 +1,64 @@
+
+const ImageFilesRepository = require('./imageFilesRepository.js');
+const UsersRepository = require('./usersRepository');
+const ImageModel = require('./image.js');
+const utilities = require("../utilities");
+const { locale } = require('date-and-time');
+const User = require('./user.js');
+const HttpContext = require('../httpContext').get();
+
+module.exports =
+    class ImagesRepository extends require('./repository') {
+        constructor() {
+            super(new ImageModel(), true /* cached */);
+            this.setBindExtraDataMethod(this.bindImageURL);
+        }
+        bindImageURL(image) {
+            if (image) {
+                let bindedImage = { ...image };
+                if (image["GUID"] != "") {
+                    bindedImage["OriginalURL"] = HttpContext.host + ImageFilesRepository.getImageFileURL(image["GUID"]);
+                    bindedImage["ThumbnailURL"] = HttpContext.host + ImageFilesRepository.getThumbnailFileURL(image["GUID"]);
+                } else {
+                    bindedImage["OriginalURL"] = "";
+                    bindedImage["ThumbnailURL"] = "";
+                }
+                if(image["UserId"]!= 0)
+                {
+                let userRepository = new UsersRepository();
+                const user = userRepository.get(image["UserId"]);
+                 if(user)
+                 {
+                    bindedImage["Username"]=user.Name;
+                    bindedImage["AvatarURL"]=HttpContext.host + ImageFilesRepository.getImageFileURL(user["AvatarGUID"]);;
+                 }   
+                }
+                return bindedImage;
+            }
+            return null;
+        }
+        add(image) {
+            if (this.model.valid(image)) {
+                image["GUID"] = ImageFilesRepository.storeImageData("", image["ImageData"]);
+                delete image["ImageData"];
+                return this.bindImageURL(super.add(image));
+            }
+            return null;
+        }
+        update(image) {
+            if (this.model.valid(image)) {
+                image["GUID"] = ImageFilesRepository.storeImageData(image["GUID"], image["ImageData"]);
+                delete image["ImageData"];
+                return super.update(image);
+            }
+            return false;
+        }
+        remove(id) {
+            let foundImage = super.get(id);
+            if (foundImage) {
+                ImageFilesRepository.removeImageFile(foundImage["GUID"]);
+                return super.remove(id);
+            }
+            return false;
+        }
+    }
